a0f896f0258ba6676ef1a65d8bc7a0f2298e48b0
David Fernandes da Costa <david.dacostabm@gmail.com>
2023-09-08 16:58:58 -0300

    Criação do arquivo

---
+++ operadores_relacionais.dart
@@ -0,0 +1,36 @@
+void main(){
+print('Operador de equidade 42 == 42 (==)');
+print(42 == 42);
+print('Operador de equidade 42 == 43 (==)');
+print(42 == 43);
+
+print('------------------');
+print('Operador de diferença 42 != 42 (!=)');
+print(42 != 42);
+print('Operador de diferença 42 != 43 (!=)');
+print(42 != 43);
+
+print('------------------');
+print('Operador de maior 42 > 42 (>)');
+print(42 > 42);
+print('Operador de 42 > 43 (>)');
+print(42 > 43);
+
+print('------------------');
+print('Operador de maior ou igual 42 >= 42');
+print(42 >= 42);
+print('Operador de maior ou igual 42 >= 43');
+print(42 >= 43);
+
+print('------------------');
+print('Operador de menor 42 < 42 (<)');
+print(42 < 42);
+print('42 < 43');
+print(42 < 43);
+
+print('------------------');
+print('Operador de menor ou igual 42 <= 42 (<=)');
+print(42 <= 42);
+print('42 <= 43');
+print(42 <= 43);  
+}
