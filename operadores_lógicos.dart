569ea1fed2add3b6edfdb72d7843da40f294ab44
David Fernandes da Costa <david.dacostabm@gmail.com>
2023-09-08 17:42:49 -0300

    Criação do arquivo

---
+++ operadores_lógicos.dart
@@ -0,0 +1,17 @@
+void main(){
+
+print('Operador lógico AND 42 == 42 && 10 == 10 (&&)');
+print(42 == 42 && 10 == 10);
+print('42 == 43 && 43 == 43');
+print(42 == 43 && 43 == 43);
+
+print('Operador lógico OR 42 == 42 || 10 == 10 (||)');
+print(42 == 42 || 10 == 10);
+print('42 == 43 || 43 == 43');
+print(42 == 43 || 43 == 45);
+
+print('Operador lógico ! 42 == 42 (!)');
+print(!(42 == 42));
+print('42 == 43');
+print(!(42 == 43));
+}
