a04593f3845dce66041edc0453662ef2391a172e
David Fernandes da Costa <david.dacostabm@gmail.com>
2023-09-08 16:34:45 -0300

    Update operadores_aritméticos.dart

--- operadores_aritméticos.dart
+++ operadores_aritméticos.dart
@@ -1,3 +1,30 @@
 void main(){
-  print("Teste");
+  print('Adição e concatenação 40 + 2, operador (+).');
+  print(40 + 2);
+  print('40' + '2');
+
+  print('---------------------');
+  print('Subtração 50 - 8, operador (-).');
+  print(50 - 8);
+
+  print('---------------------');
+  print('Negação, inverte o sinal da operação -(+42),');
+  print(-(42));
+
+  print('---------------------');
+  print('Multiplicação 6 * 7, operador (*).');
+  print('6 * 7');
+
+  print('---------------------');
+  print('Divisão 11 / 2, operador (/).');
+  print(11 / 2);
+
+  print('---------------------');
+  print('Divisão com retor da parte inteira 11 ~/2, operador (~/).');
+  print(11~/2);
+
+  print('---------------------');
+  print('Restornar o resto da divisão 11 % 2, operador (%).');
+  print(11 % 2);
+
 }
