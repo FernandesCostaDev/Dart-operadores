70e7ad440a3b802af8cb5131cab12fff954f23a9
David Fernandes da Costa <david.dacostabm@gmail.com>
2023-09-09 10:02:38 -0300

    Criação do arquivo

---
+++ 4_manipuladores_bits_2.dart
@@ -0,0 +1,104 @@
+void main(){
+  
+  var num = 64;
+  var shift = num >> 1;
+
+  print('valor da variável num:');
+  print(num);
+
+  print('');
+
+  print('valor da variável num com o operador de deslocamento uma casa a direita (>>):');
+  print(shift);
+  print(shift.toRadixString(2).padLeft(8,'0'));
+  print('------------------');
+
+  shift = shift >> 1;
+
+  print('valor da variável num com o operador de deslocamento uma casa a direita (>>):');
+  print(shift);
+  print(shift.toRadixString(2).padLeft(8,'0'));
+  print('------------------');
+
+  shift = shift >> 1;
+
+  print('valor da variável num com o operador de deslocamento uma casa a direita (>>):');
+  print(shift);
+  print(shift.toRadixString(2).padLeft(8,'0'));
+  print('------------------');
+
+  shift = shift >> 1;
+
+  print('valor da variável num com o operador de deslocamento uma casa a direita (>>):');
+  print(shift);
+  print(shift.toRadixString(2).padLeft(8,'0'));
+  print('------------------');
+
+  shift = shift >> 1;
+
+  print('valor da variável num com o operador de deslocamento uma casa a direita (>>):');
+  print(shift);
+  print(shift.toRadixString(2).padLeft(8,'0'));
+  print('------------------');
+
+  shift = shift >> 1;
+
+  print('valor da variável num com o operador de deslocamento uma casa a direita (>>):');
+  print(shift);
+  print(shift.toRadixString(2).padLeft(8,'0'));
+  
+  print('--------------------------------------------');
+
+  print('Realizadno a operação com bits de forma inversa com operador de bits(<<)');
+
+  var num2 = 1;
+  var shift2 = num2 << 1;
+
+
+  print('valor da variável num2:');
+  print(num2);
+
+
+  print('valor da variável num2 com o operador de deslocamento uma casa a esquerda (<<):');
+  print(shift2);
+  print(shift2.toRadixString(2).padLeft(8,'0'));
+  print('------------------');
+
+  shift2 = shift2 << 1;
+
+  
+  print('valor da variável num2 com o operador de deslocamento uma casa a esquerda (<<):');
+  print(shift2);
+  print(shift2.toRadixString(2).padLeft(8,'0'));
+  print('------------------');
+
+  shift2 = shift2 << 1;
+
+  print('valor da variável num2 com o operador de deslocamento uma casa a esquerda (<<):');
+  print(shift2);
+  print(shift2.toRadixString(2).padLeft(8,'0'));
+  print('------------------');
+
+  shift2 = shift2 << 1;
+
+  print('valor da variável num2 com o operador de deslocamento uma casa a esquerda (<<):');
+  print(shift2);
+  print(shift2.toRadixString(2).padLeft(8,'0'));
+  print('------------------');
+
+shift2 = shift2 << 1;
+
+ print('valor da variável num2 com o operador de deslocamento uma casa a esquerda (<<):');
+  print(shift2);
+  print(shift2.toRadixString(2).padLeft(8,'0'));
+  print('------------------');
+
+shift2 = shift2 << 1;
+
+ print('valor da variável num2 com o operador de deslocamento uma casa a esquerda (<<):');
+  print(shift2);
+  print(shift2.toRadixString(2).padLeft(8,'0'));
+  print('------------------');
+
+
+}
